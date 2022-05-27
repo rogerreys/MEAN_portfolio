70122af5281b973810f849df28f7b56194ef02c1
Roger-Ryes <roger.reyesm97@gmail.com>
2022-05-26 22:14:43 -0500

    Estructyra AboutMe

---
+++ src/app/portfolio/interface/skills.ts
@@ -0,0 +1,4 @@
+export interface Skills {
+    title:string,
+    skills: string[]
+}
--- src/app/portfolio/pages/about-me/about-me.component.ts
+++ src/app/portfolio/pages/about-me/about-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Skills } from '../../interface/skills';
 
 @Component({
   selector: 'app-about-me',
@@ -7,6 +8,21 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class AboutMeComponent implements OnInit {
+
+  aboutMeParagraph: string = `Hola!!! Me llamo Roger Reyes y soy un desarrollador web fullstack junior, en constante aprendizaje y con el deseo de desarrollar grandes proyectos 
+  para solucionar problemas.
+  Dispuesto a trabajar en grupo para lograr objetivos mutuos.
+  Tambien, llevo algunos años programando pero ultimamente me he dedicado al mundo del desarrollo web.`;
+
+  abilities: Skills[] = [
+    { title: "Lenguajes de Marcado", skills: ["CSS Grid / Flex", "HTML"] },
+    { title: "Lenguajes de programacion", skills: ["PHP", "JavaScript", "TypeScript", "Python", "C#"] },
+    { title: "Base de datos", skills: ["SQL / MySQL", "Mongoos"] },
+    { title: "Librerias", skills: ["Bootstrap", "jQuery"] },
+    { title: "Framework", skills: ["node JS", "Express", "Laravel", "Angular"] }
+  ]
+
+
 
   constructor() { }
 
